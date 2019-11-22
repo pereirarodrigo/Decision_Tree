68023b0b6bd20ff931ba0d9faa3d88f5fd24c33a
Rodrigo <prehistoriclife65@gmail.com>
2019-11-22 17:53:22 -0300

    Update decision_tree.py

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import export_graphviz
-from sklearn.externals.six import StringIO  # Depreciado, exibirá um alerta
+from sklearn.externals.six import StringIO  # Depreciado, exibirá um alerta | Deprecated, will show an alert
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from IPython.display import Image
